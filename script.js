2ba34178edeffc255ea5c6e954ac432cfea47e11
LaraCosta66 <larasmila64@gmail.com>
2023-02-22 16:53:02 -0300

    feat: add div and styles

---
+++ script.js
@@ -0,0 +1,71 @@
+const container = document.querySelector("#container");
+const gridDiv = document.createElement('div')
+const div2 = document.createElement('div')
+const div3 = document.createElement('div')
+const div4 = document.createElement('div')
+const div5 = document.createElement('div')
+const div6 = document.createElement('div')
+const div7 = document.createElement('div')
+const div8 = document.createElement('div')
+const div9 = document.createElement('div')
+const div10 = document.createElement('div')
+const div11 = document.createElement('div')
+const div12 = document.createElement('div')
+const div13 = document.createElement('div')
+const div14 = document.createElement('div')
+const div15 = document.createElement('div')
+const div16 = document.createElement('div')
+
+container.appendChild(gridDiv)
+gridDiv.classList.add('one')
+gridDiv.textContent = 'div 1'
+
+
+container.appendChild(div2)
+div2.textContent = 'div 2'
+container.appendChild(div3)
+div3.textContent = 'div 3'
+container.appendChild(div4)
+div4.textContent = 'div 4'
+container.appendChild(div5)
+div5.textContent = 'div 5'
+container.appendChild(div6)
+div6.textContent = 'div 6'
+container.appendChild(div7)
+div7.textContent = 'div 7'
+container.appendChild(div8)
+div8.textContent = 'div 8'
+container.appendChild(div9)
+div9.textContent = 'div 9'
+container.appendChild(div10)
+div10.textContent = 'div 10'
+container.appendChild(div11)
+div11.textContent = 'div 11'
+container.appendChild(div12)
+div12.textContent = 'div 12'
+container.appendChild(div13)
+div13.textContent = 'div 13'
+container.appendChild(div14)
+div14.textContent = 'div 14'
+container.appendChild(div15)
+div15.textContent = 'div 15'
+container.appendChild(div16)
+div16.textContent = 'div 16'
+
+div2.classList.add('two')
+div3.classList.add('three')
+div4.classList.add('four')
+div5.classList.add('five')
+div6.classList.add('six')
+div7.classList.add('seven')
+div8.classList.add('eight')
+div9.classList.add('nine')
+div10.classList.add('ten')
+div11.classList.add('eleven')
+div12.classList.add('twelve')
+div13.classList.add('thirteen')
+div14.classList.add('fouteen')
+div15.classList.add('fifteen')
+div16.classList.add('sixteen')
+
+
